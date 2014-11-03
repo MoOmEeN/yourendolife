febb95faab619fa5954c2f68f4f94d553f5c363e
Radek <radek@barca>
2014-11-04 00:15:10 +0100

    theme change to blue + uiscope session holder

--- src/main/java/com/moomeen/EndomondoSessionHolder.java
+++ src/main/java/com/moomeen/EndomondoSessionHolder.java
@@ -1,10 +1,12 @@
 package com.moomeen;
 
 import org.springframework.stereotype.Service;
+import org.vaadin.spring.UIScope;
 
 import com.moomeen.endo2java.EndomondoSession;
 
 @Service
+@UIScope
 public class EndomondoSessionHolder {
 
 	private com.moomeen.endo2java.EndomondoSession internalSession;
