f45d2e7d558d2959233d920a92bfb244505948cf
Radek <radek@hotelninjas.com>
2015-01-12 17:25:45 +0100

    footer text improvement

--- src/main/java/com/moomeen/views/Footer.java
+++ src/main/java/com/moomeen/views/Footer.java
@@ -7,6 +7,7 @@ import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Link;
 import com.vaadin.ui.Panel;
@@ -18,44 +19,62 @@ import com.vaadin.ui.Window;
 public class Footer extends VerticalLayout {
 	
 	public Footer() {
-		Panel panel = new Panel();
-		
-		VerticalLayout footerLayout = new VerticalLayout();
-		panel.setContent(footerLayout);
-		
 		setStyleName("footer");
 		
-		footerLayout.setSpacing(true);
-		footerLayout.setMargin(true);
-		
-		Link email = new Link(CONTACT_EMAIL, new ExternalResource("mailto:" + CONTACT_EMAIL));
-		 
-		 footerLayout.addComponent(email);
-		 footerLayout.setComponentAlignment(email, Alignment.MIDDLE_CENTER);
-		 
-		 Button feedback = new Button("feedback");
-		 
-		 footerLayout.addComponent(feedback);
-		 feedback.setStyleName("link");
-		 feedback.addClickListener(new ClickListener() {
-			
+		Panel panel = new Panel();
+		panel.setContent(getFooterMessage());
+		addComponent(panel);
+		setComponentAlignment(panel, Alignment.MIDDLE_CENTER);
+
+		Label copyright = new Label("© 2015 yourENDOlife.com");
+		copyright.setStyleName("footer-copyright");
+		addComponent(copyright);
+		setComponentAlignment(copyright, Alignment.BOTTOM_CENTER);
+	}
+
+	private Component getFooterMessage() {
+		VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+
+		Label textPartOne = new Label("Question? Suggestion? Any feedback is welcome! You can use our");
+		Button feedback = getFeedbackWindowButton("contact form");
+		Label textPartTwo = new Label("or send it directly to");
+		Link email = getEmailLink();
+	
+		add(layout, textPartOne);
+		add(layout, feedback);
+		add(layout, textPartTwo);
+		add(layout, email);
+
+		return layout;
+	}
+
+	private void add(VerticalLayout layout, Component component) {
+		layout.addComponent(component);
+		layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
+	}
+
+	private Button getFeedbackWindowButton(String text) {
+		Button feedback = new Button(text);
+		feedback.setStyleName("link");
+
+		feedback.addClickListener(new ClickListener() {
+
 			@Override
 			public void buttonClick(ClickEvent event) {
-				Window w = getFeedbackForm();
+				Window w = getFeedbackWindow();
 				UI.getCurrent().addWindow(w);
 			}
 		});
-		 
-		 addComponent(panel);
-		 setComponentAlignment(panel, Alignment.MIDDLE_CENTER);
-		 
-		 Label copyright = new Label("© 2015 yourENDOlife.com");
-		 copyright.setStyleName("footer-copyright");
-		 addComponent(copyright);
-		 setComponentAlignment(copyright, Alignment.BOTTOM_CENTER);
+		return feedback;
+	}
+
+	private Link getEmailLink() {
+		Link email = new Link(CONTACT_EMAIL, new ExternalResource("mailto:" + CONTACT_EMAIL));
+		return email;
 	}
 	
-	private Window getFeedbackForm(){
+	private Window getFeedbackWindow(){
 		Window w = new Window("Leave your feedback");
 		w.center();
 		w.setWidth("400px");
